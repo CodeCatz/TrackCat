c96c19145bf45620fe4e95fe7f6cccbb043613b4
mojcas <mojcas@users.noreply.github.com>
2014-12-06 12:05:52 +0100

    modified settings.py to fix error from python manage.py migrate   File "C:\work\TrackCat\TrackCat\settings.py", line 88     except ImportError, e:                       ^ SyntaxError: invalid syntax

--- TrackCat/settings.py
+++ TrackCat/settings.py
@@ -74,16 +74,10 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-try:
-	from settings_local import *
-except ImportError, e:
-	pass
