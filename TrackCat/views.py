45049dde48673fa8da93de199b2d9c92559df81c
anuschka <sustic@gmail.com>
2014-12-18 09:22:53 +0100

    Added empty object {} in redner_to_response

--- TrackCat/views.py
+++ TrackCat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response, render
 from django.template import RequestContext
 
 def index(request):
-    return render_to_response('pages/index.html')
+    return render_to_response('pages/index.html',{})
 
 def projects(request):
 	return render(request, 'pages/projects.html',{})
