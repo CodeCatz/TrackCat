8766353accd7ed2b972b83606b8280f83cc30302
anuschka <sustic@gmail.com>
2014-12-18 22:49:49 +0100

    Moved STATICFILES_DIRS and added space in CSS for consistency

--- TrackCat/settings.py
+++ TrackCat/settings.py
@@ -86,12 +86,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
 
 try:
     from .settings_local import *
 except ImportError as e:
     pass
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
