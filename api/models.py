5be04cb86b04e600fed7619965f3a7de6141cc0b
Petra Vidmar <pettra.vidmar@gmail.com>
2015-01-26 20:50:56 +0100

    created UserProfile model

--- api/models.py
+++ api/models.py
@@ -1,23 +1,32 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    full_name = models.CharField(max_length=100, null=False, blank=False)
-	email = models.EmailField(null=False, blank=False)
-	profile_picture = image = models.ImageField(upload_to = generate_filename, blank = True)  
-	activity_level = (
+ACTIVITY_LEVEL = (
 			("SC", "Sleepy_Cat"),
 			("LC", "Lazy_Cat"),
 			("AC", "Active_Cat"),
 			("MC", "Mentor_Cat"),
 	)
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User) 
+	#id = models.AutoField(primary_key=True) 
+	fullname = models.CharField(max_length=100, blank=False)
+	githubuser = models.CharField(max_length=100, null=False, blank=False)
+	email = models.EmailField(blank=False)
+	profile_picture = models.ImageField(blank = True)  
 	website = models.URLField(null=False)
-	bio = models.TextField(max_length=1000,blank=True)
-	date_joined = models.DateTimeField(auto_now_add=True)
-	date_of_last_login = models.DateTimeField(auto_now=True)
-	date_update = models.DateTimeField(auto_now_add=True)
+	bio = models.TextField(max_length=2000,blank=True)
 	active = models.BooleanField(default=False)
-	project = models.ManyToManyField(Project, related_name="project_name")
-	task = models.ManyToManyField(Tasks, related_name="title")
+	activity_status = models.CharField(max_length=2, choices=ACTIVITY_LEVEL)
+	last_logged_in = models.DateTimeField(auto_now=True, blank=False)
+	date_created = models.DateTimeField(auto_now_add=True, blank=False)
+	date_updated = models.DateTimeField(auto_now_add=True)
+
+	def publish(self):
+		self.date_created = timezone.now()
+		self.save()
+
+	def __unicode__(self):
+		return self.fullname
