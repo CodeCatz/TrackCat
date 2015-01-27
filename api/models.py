e8940df1fac3531b0eb3ca85a22144a6489a99d0
anuschka <sustic@gmail.com>
2015-01-27 09:05:48 +0100

    updated field names and default values

--- api/models.py
+++ api/models.py
@@ -2,20 +2,19 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 
-
 class Projects(models.Model):
 	PROJECT_STATUS = (
-		('O', 'Opened'),
-		('I', 'In progress'),
-		('C', 'Closed'),
-	)
+		('open', 'Opened'),
+		('progress', 'In progress'),
+		('closed', 'Closed'),
+		)
 	project_name = models.CharField(max_length=50)
 	project_description = models.CharField(max_length=150, blank=True)
-	status_id = models.CharField(max_length=1, choices=PROJECT_STATUS, blank=True)
+	status_id = models.CharField(max_length=8, choices=PROJECT_STATUS, default=open)
 	project_deadline = models.DateTimeField(blank=True)
-	repository_link = models.URLField(max_length=200, blank=True)
-	website_prod = models.URLField(max_length=200, blank=True)
-	website_test = models.URLField(max_length=200, blank=True)
-#	project_owner = models.ForeignKey(Users)
+	repository_link = models.CharField(max_length=300, blank=True)
+	website_prod = models.CharField(max_length=300, blank=True)
+	website_test = models.CharField(max_length=300, blank=True)
+	# project_owner = models.ForeignKey(Users)
 	date_created = models.DateTimeField(auto_now_add=True, default=timezone.now)
-	date_modified = models.DateTimeField(auto_now=True, default=timezone.now)
+	date_modified = models.DateTimeField(auto_now=True)
