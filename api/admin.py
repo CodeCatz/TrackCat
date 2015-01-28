2fbcebb6b6a19822aa28f0a2027386283e311cbd
Livia Dobai <livia.dobai@gmail.com>
2015-01-28 20:38:44 +0100

    Siplified import

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Task
-from .models import UserProfile
-from .models import Project
+from .models import *
 
 admin.site.register(Task)
 admin.site.register(UserProfile)
