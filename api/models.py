3e01bfd665e6f51f37009aa2834b72feb4db88f5
anuschka <sustic@gmail.com>
2015-01-28 13:33:08 +0100

    Used upper case for constants

--- api/models.py
+++ api/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Projects(models.Model):
 	PROJECT_STATUS = (
-		('open', 'Opened'),
-		('progress', 'In progress'),
-		('closed', 'Closed'),
+		('OPENED', 'Opened'),
+		('INPROGRESS', 'In progress'),
+		('CLOSED', 'Closed'),
 		)
 	project_id = models.AutoField(primary_key=True)
 	project_name = models.CharField(max_length=50)
 	project_description = models.CharField(max_length=150, blank=True)
-	status_id = models.CharField(max_length=8, choices=PROJECT_STATUS, default=open)
+	status_id = models.CharField(max_length=8, choices=PROJECT_STATUS, default='OPENED')
 	project_deadline = models.DateTimeField(blank=True)
 	repository_link = models.URLField(max_length=300)
 	website_production = models.URLField(max_length=300, blank=True)
