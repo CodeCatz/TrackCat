ff1cd9cafcc46d203e819eea1579185785efe3db
anuschka <sustic@gmail.com>
2015-02-04 19:46:31 +0100

    Added github social auth

--- TrackCat/settings.py
+++ TrackCat/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'social.apps.django_app.default',
 
 	# defined apps
 	'web',
@@ -55,9 +56,16 @@ MIDDLEWARE_CLASSES = (
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+AUTHENTICATION_BACKENDS = (
+	'social.backends.github.GithubOAuth2',
+	'django.contrib.auth.backends.ModelBackend',
+)
+
 TEMPLATE_CONTEXT_PROCESSORS = (
 	'django.contrib.auth.context_processors.auth',
 	'django.core.context_processors.request',
+	'social.apps.django_app.context_processors.backends',
+	'social.apps.django_app.context_processors.login_redirect',
 )
 
 ROOT_URLCONF = 'TrackCat.urls'
@@ -100,3 +108,14 @@ try:
 	from .settings_local import *
 except ImportError as e:
 	pass
+
+AUTH_PROFILE_MODULE = 'api.UserProfile'
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_ERROR_URL = '/login-error/'
+SOCIAL_AUTH_NEW_USER_REDIRECT_URL = '/'
+SOCIAL_AUTH_ENABLED_BACKENDS = ('github')
+SOCIAL_AUTH_DEFAULT_USERNAME = 'new_social_auth_user'
+SOCIAL_AUTH_GITHUB_KEY = ''
+SOCIAL_AUTH_GITHUB_SECRET = ''
+
