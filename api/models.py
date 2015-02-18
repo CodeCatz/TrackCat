941fa6319f5ad78f515514a8aa577f997c1c5ae5
Livia Dobai <livia.dobai@gmail.com>
2015-02-18 20:26:45 +0100

    fixes broken project form

--- api/models.py
+++ api/models.py
@@ -2,15 +2,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+ACTIVITY_LEVEL_CHOICES = (
+		("SC", "Sleepy_Cat"),
+		("LC", "Lazy_Cat"),
+		("AC", "Active_Cat"),
+		("MC", "Mentor_Cat"),
+)
+
 class UserProfile(models.Model):
-
-	ACTIVITY_LEVEL = (
-			("SC", "Sleepy_Cat"),
-			("LC", "Lazy_Cat"),
-			("AC", "Active_Cat"),
-			("MC", "Mentor_Cat"),
-	)
-
 	user = models.OneToOneField(User) 
 	#id = models.AutoField(primary_key=True) 
 	fullname = models.CharField(max_length=100, blank=False)
@@ -20,23 +19,23 @@ class UserProfile(models.Model):
 	website = models.URLField(blank = True)
 	bio = models.TextField(max_length=2000,blank=True)
 	active = models.BooleanField(default=False)
-	activity_status = models.CharField(max_length=2, choices=ACTIVITY_LEVEL)
+	activity_status = models.CharField(max_length=2, choices=ACTIVITY_LEVEL_CHOICES)
 	last_logged_in = models.DateTimeField(auto_now=True, blank=False)
 	date_created = models.DateTimeField(auto_now_add=True, blank=False)
 	date_updated = models.DateTimeField(auto_now_add=True)
 
+
+PROJECT_STATUS_CHOICES = (
+	('OPENED', 'Opened'),
+	('INPROGRESS', 'In progress'),
+	('CLOSED', 'Closed'),
+)
+
 class Project(models.Model):
-	
-	PROJECT_STATUS = (
-		('OPENED', 'Opened'),
-		('INPROGRESS', 'In progress'),
-		('CLOSED', 'Closed'),
-	)
-	
 	project_id = models.AutoField(primary_key=True)
 	project_name = models.CharField(max_length=50)
 	project_description = models.CharField(max_length=150, blank=True)
-	status_id = models.CharField(max_length=15, choices=PROJECT_STATUS, default='OPENED')
+	status_id = models.CharField(max_length=15, choices=PROJECT_STATUS_CHOICES, default='OPENED')
 	project_deadline = models.DateTimeField(blank=True, null=True)
 	repository_link = models.URLField(max_length=300)
 	website_production = models.URLField(max_length=300, blank=True)
@@ -46,15 +45,14 @@ class Project(models.Model):
 	date_modified = models.DateTimeField(auto_now=True)
 	user_project = models.ManyToManyField(UserProfile, related_name='user_project')
 
+STATUS_CHOICES = (
+	('UNASSIGNED', 'Unassigned'),
+	('ASSIGNED', 'Assigned'),
+	('WORKINGON', 'Working On'),
+	('COMPLETED', 'Completed'),
+)
+
 class Task (models.Model):
-
-	STATUS_CHOICES = (
-		('UNASSIGNED', 'Unassigned'),
-		('ASSIGNED', 'Assigned'),
-		('WORKINGON', 'Working On'),
-		('COMPLETED', 'Completed'),
-	)
-
 	title = models.CharField(max_length=255)
 	description = models.TextField(max_length=1000, blank=True)
 	status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='UNASSIGNED')
