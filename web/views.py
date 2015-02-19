89bb81aefc5c5c50d6511499b3dc0c71dcf3d2aa
Tjasa Zajc <tjasa.zajc@gmail.com>
2015-02-19 19:30:23 +0100

    Created tasks page listing tasks and linking to project details if you click the name of the task under Part of project section

--- web/views.py
+++ web/views.py
@@ -5,6 +5,7 @@ from .form import ProjectForm
 from django.contrib.auth import logout as auth_logout
 from api.models import Project
 from api.models import UserProfile
+from api.models import Task
 
 def index(request):
 	return render_to_response('pages/index.html',{})
@@ -19,6 +20,10 @@ def about(request):
 def members(request):
 	members_list = UserProfile.objects.filter(active=True).order_by('fullname')
 	return render(request, 'pages/members.html',{'members_list': members_list})
+
+def tasks(request):
+	task_list = Task.objects.all()
+	return render(request, 'pages/tasks.html', {'task_list': task_list})
 
 def events(request):
 	return render(request, 'pages/events.html',{})
@@ -55,7 +60,7 @@ def project_new(request):
 
 def project_edit(request, project_id):
 	project = get_object_or_404(Project, project_id=project_id)
-	
+
 	if request.method == "POST":
 		projectform = ProjectForm(request.POST, instance=project)
 		if projectform.is_valid():
