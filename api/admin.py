f67ef289dc049b20a9fbdc36cd528248850d9c2c
anuschka <sustic@gmail.com>
2015-02-21 20:36:30 +0100

    fixed indetation in admin.py

--- api/admin.py
+++ api/admin.py
@@ -4,8 +4,8 @@ from .models import *
 class UserProfileAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,				{'fields': ['user', 'fullname', 'githubuser', 'email','website']}),
-									('Biography',		{'fields': ['bio']}),
-									('Activity',		{'fields': ['active','activity_status']}),
+		('Biography',		{'fields': ['bio']}),
+		('Activity',		{'fields': ['active','activity_status']}),
 		]
 	list_display = ('user', 'fullname', 'githubuser', 'activity_status')
 	list_filter = ('activity_status',)
