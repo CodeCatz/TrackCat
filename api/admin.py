f9d406ccb13fc3400ba642fc881edd2710178552
anuschka <sustic@gmail.com>
2015-02-21 22:25:00 +0100

    Customized projects and tasks in admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+class ProjectInline(admin.TabularInline):
+	model = Project
+
 class UserProfileAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,				{'fields': ['user', 'fullname', 'githubuser', 'email','website']}),
@@ -9,8 +12,27 @@ class UserProfileAdmin(admin.ModelAdmin):
 		]
 	list_display = ('user', 'fullname', 'githubuser', 'activity_status')
 	list_filter = ('activity_status',)
+	inlines = [
+		ProjectInline,
+		]
 
-admin.site.register(Task)
+class ProjectAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,				{'fields': ['project_name', 'status_id', 'project_owner']}),
+		('Project details',	{'fields': ['project_description', 'project_deadline', 'repository_link', 'website_production', 'website_test', ]}),
+		]
+	list_display = ('project_name', 'status_id', 'project_deadline',)
+	list_filter = ('status_id',)
+
+class TaskAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,				{'fields': ['title', 'status', 'owner_id', 'project_id']}),
+		('Task details',	{'fields': ['description', 'deadline', ]}),
+		]
+	list_display = ('title', 'status', 'assigned_id',)
+	list_filter = ('assigned_id',)
+
+admin.site.register(Task, TaskAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
-admin.site.register(Project)
+admin.site.register(Project, ProjectAdmin)
 
