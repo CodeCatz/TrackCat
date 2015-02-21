10b936ba9e52db51190288e46bac66d70a3af347
anuschka <sustic@gmail.com>
2015-02-21 20:32:41 +0100

    Fixed indetation and added a space in admin.py

--- api/admin.py
+++ api/admin.py
@@ -4,12 +4,13 @@ from .models import *
 class UserProfileAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,				{'fields': ['user', 'fullname', 'githubuser', 'email','website']}),
-        ('Biography',		{'fields': ['bio']}),
-        ('Activity',		{'fields': ['active','activity_status']}),
-    ]
+									('Biography',		{'fields': ['bio']}),
+									('Activity',		{'fields': ['active','activity_status']}),
+		]
 	list_display = ('user', 'fullname', 'githubuser', 'activity_status')
 	list_filter = ('activity_status',)
 
 admin.site.register(Task)
-admin.site.register(UserProfile,UserProfileAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Project)
+
