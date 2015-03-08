c183f3090853911c8afb316f7ed8a17aabbed15f
anuschka <sustic@gmail.com>
2015-03-08 11:27:05 +0100

    my tasks showing assigned and owned tasks for the loged in user

--- web/views.py
+++ web/views.py
@@ -110,8 +110,9 @@ def project_delete(request, project_id):
 
 @login_required
 def tasks(request):
-	task_list = Task.objects.filter(project_id__in=Project.objects.exclude(status='DELETED')).filter(owner_id=request.user).exclude(status = 'DELETED')
-	return render(request, 'pages/tasks.html', {'task_list': task_list})
+	task_owned = Task.objects.filter(project_id__in=Project.objects.exclude(status='DELETED')).filter(owner_id=request.user).exclude(status = 'DELETED')
+	task_assigned = Task.objects.filter(project_id__in=Project.objects.exclude(status='DELETED')).filter(assigned_id=request.user).exclude(status = 'DELETED')
+	return render(request, 'pages/tasks.html', {'task_owned': task_owned, 'task_assigned': task_assigned})
 
 @permission_required('api.change_task', login_url='/login/')
 def task_edit(request, task_id):
