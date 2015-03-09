8e6cacf0a18cbbe8bdb901abac81a8743bd18a26
TrackCat <trackcatz@gmail.com>
2015-03-09 15:30:35 +0100

    configured wsgi

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn mysite.wsgi
+web: gunicorn TrackCat.wsgi
 
--- TrackCat/wsgi.py
+++ TrackCat/wsgi.py
@@ -16,3 +16,7 @@ application = get_wsgi_application()
 from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
 
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
